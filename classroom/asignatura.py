d770531424e39feb1fde2ac17179947c5366a6e7
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-09-04 22:45:58 -0500

    Update asignatura.py

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -3,6 +3,6 @@ class Asignatura:
     def __init__(self, nombre, salon):
         self._nombre = nombre
         self._salon = salon
-
-    # def __str__(self):
-    #     pass
+        
+    def __str__(self):
+        return self._nombre + " " + self._salon
